f538fecf7339932da20ee9f0104f7b2d9df8b7d1
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-07-01 23:34:03 +0000

    breaking api changes to make (event, context) also route handler arguments. also added handler() method so that user does not have to call dispatch themself

--- examples/handler.js
+++ examples/handler.js
@@ -1,20 +1,47 @@
 'use strict'
 
-const app = require('@funcmatic/lambda-router')
-//const app = new Router()
+// Example of an AWS Node.js 8.10 Lambda function that 
+// uses the lambda-router
 
-app.get('/hello/:userid', async ({ event, context, func }, params) => {
-  func.log.info("pino logger!")
-  var res = (await func.request.get('danieljyoo/funcmatic-helloworld')).data
-  var ret = { 'hello': 'world', params: params }
-  return ret
+// First install the package in your project: 
+// $> npm install --save @funcmatic/router
+
+// Reqiure the router in your project which creates
+// an instance of the router
+const app = require('@funcmatic/lambda-router')
+
+// Example of defining a route which matches HTTP GET requests such as
+// * GET /hello/1234
+// * GET /hello/danieljyoo
+// but NOT
+// * GET /hello/
+// * POST /hello/1234
+app.get('/hello/:userid', async (event, context) => {
+  // the router will place any path parameters (e.g. userid) 
+  // in the context.router.params 
+  // If there are no path params, 
+  // context.router.params will be null.
+  // In this case, of the GET /hello/123 
+  // context.router.params will have the value:
+  // { 
+  //   userid: "123"
+  // }
+  // Note that "123" is of type string
+  var pathParams = context.router.params
+  
+  // return using API Gateway's Lamba Proxy Integration 
+  // JSON response format. 
+  return { 
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(pathParams),
+    isBase64Encoded: false
+  }
 })  
 
-module.exports.handler = async (event, context) => {  
-  await app.dispatch(event.event.method, event.event.path, { 
-      event: event.event, 
-      context: event.context,
-      func: { log, request, error }
-    })
-}
-  
+// app.handler() returns a function with structure:
+// async (event, context) => { ... } 
+// which will be invoked by AWS Lambda
+module.exports.handler = app.handler()
--- test/router.test.js
+++ test/router.test.js
@@ -5,30 +5,39 @@ describe('Router', () => {
   it ('create a new router', async () => {
     var options = { }
     var router = app.create(options)
-    router.get('/hello/:userid', (ctx) => {
+    router.get('/hello/:userid', (event, context) => {
       return { 
         'hello': 'world',
-        'ctx': ctx
+        event, 
+        context
       }
     })
-    router.get('')
-    
-    var ctx = { 'foo': 'bar' }
-    var res = await router.dispatch('GET', '/hello/123', ctx)
-    expect(res.ctx).toMatchObject({
+    var event = {
+       method: 'GET', 
+       path: '/hello/123'
+    }
+    var context = { 'foo': 'bar' }
+    var res = await router.handler(event, context)
+    expect(res.context).toMatchObject({
       'foo': 'bar',
-      'params': {
-        'userid': '123'
+      'router': { 
+        'params': {
+          'userid': '123'
+        }
       }
     })
   })
   it ('should return and unhandled error if no matching route', async () => {
     var options = { }
     var router = app.create(options)
-    router.get('/hello/:userid', (ctx) => {
+    router.get('/hello/:userid', (event, context) => {
       return { 'hello': 'world' }
     })
-    var res = await router.dispatch('GET', '/', { })
+    var event = {
+      method: 'GET',
+      path: '/'
+    }
+    var res = await router.handler(event, { })
     expect(res instanceof Error).toBe(true)
   })
 }) 
